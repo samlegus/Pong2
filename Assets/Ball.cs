f38b15a3b3c7361625ae26ba268ea569321d301b
Sam Legus <sam.legus@aie.edu>
2022-07-13 10:36:20 -0700

    3

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -6,11 +6,13 @@ public class Ball : MonoBehaviour
 {
     public float speed = 5.25f;
     public Rigidbody2D rb;
+    public GameManager manager; 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = Vector2.left * speed;
+        manager = GameObject.FindObjectOfType<GameManager>();
     }
 
     //Calculate angle for ball
@@ -23,7 +25,7 @@ public class Ball : MonoBehaviour
     {
         //Get the actual name of the thing we are hitting
         string colName = collision.gameObject.name;
-        if(colName == "PlayerOne")
+        if (colName == "PlayerOne")
         {
             float y = HitFactor(transform.position, collision.transform.position, collision.collider.bounds.size.y);
             Vector2 dir = new Vector2(1, y).normalized; //direction
@@ -33,6 +35,21 @@ public class Ball : MonoBehaviour
         {
             float y = HitFactor(transform.position, collision.transform.position, collision.collider.bounds.size.y);
             Vector2 dir = new Vector2(-1, y).normalized; //direction
+            rb.velocity = dir * speed;
+        }
+        if (colName == "WallLeft")
+        {
+            //manager.scoreB = manager.scoreB + 1;
+            manager.scoreB += 1;
+            transform.position = new Vector3(0f, 0f, 0f); //Re-center the ball
+            Vector2 dir = new Vector2(1f, 0);
+            rb.velocity = dir * speed;
+        }
+        if (colName == "WallRight")
+        {
+            manager.scoreA += 1;
+            transform.position = new Vector3(0f, 0f, 0f);
+            Vector2 dir = new Vector2(-1f, 0);
             rb.velocity = dir * speed;
         }
     }
